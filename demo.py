4934d1de4357e022555e43c3ad6d198308043cf5
Keith Emmanuel <whatwhatoh@gmail.com>
2018-09-06 14:47:27 -0700

    Adding data intake and processing functions

--- demo.py
+++ demo.py
@@ -1,1 +1,12 @@
-print 'Hello World'
+def take_in_data():
+    print 'taking in data'
+
+def process_data():
+    print 'processing the data'
+
+def print_data():
+    print 'printing the data'
+
+
+take_in_data()
+process_data()
